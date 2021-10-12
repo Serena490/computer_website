3ca728606025cd8b8fdae11e7a4e70ecc1b6e035
Serena490 <serenagasper490@gmail.com>
2021-10-12 12:38:22 +0100

    the code for form

---
+++ ShoppingBasket/AnimalProducts.js
@@ -0,0 +1,52 @@
+var items = {
+    123: {
+        name: "Dog Guide"
+        info: "Read up on basic information to help take care of your pets and purchase our guide"
+        img: "INSERT  IMAGE"
+    },  
+}
+
+var items = {
+    124: {
+        name: "Cat Guide"
+        info: "Read up on basic information to help take care of your pets and purchase our guide"
+        img: "INSERT  IMAGE"
+    price : 2.50
+    },  
+}
+
+var items = {
+    125: {
+        name: "Rabbit Guide"
+        info: "Read up on basic information to help take care of your pets and purchase our guide"
+        img: "INSERT  IMAGE"
+     price : 2.50
+    },  
+}
+
+var items = {
+    123: {
+        name: "Rodent Guide"
+        info: "Read up on basic information to help take care of your pets and purchase our guide"
+        img: "INSERT  IMAGE"
+    price : 2.50
+    },  
+}
+
+var items = {
+    123: {
+        name: "Dog Bed"
+        info: "Read up on basic information to help take care of your pets and purchase our guide"
+        img: "INSERT  IMAGE"
+     price : 2.50
+    },  
+}
+
+var items = {
+    123: {
+        name: "Cat Bed"
+        info: "Read up on basic information to help take care of your pets and purchase our guide"
+        img: "INSERT  IMAGE"
+    price : 2.50
+    },  
+}
